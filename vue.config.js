cf002d7333aaec34d8f7714b2304db79d7f264f5
LLanQing <44639217+LLanQing@users.noreply.github.com>
2022-08-01 21:33:00 +0800

    vue配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+	transpileDependencies: true,
+	// 关闭代码保存校验
+	lintOnSave: false,
+});
