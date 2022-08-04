c6cf72ad510ca5158e576cda5bad320137c994ce
LLanQing <44639217+LLanQing@users.noreply.github.com>
2022-08-04 22:03:28 +0800

    内置组件teleprot

---
+++ 18src_内置组件teleprot/components/Son.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="son">
+		<h2>son(孙)</h2>
+		<Dialog></Dialog>
+	</div>
+</template>
+
+<script>
+import Dialog from "./Dialog.vue";
+export default {
+	name: "Son",
+	components: { Dialog },
+};
+</script>
+
+<style scoped>
+.son {
+	background: orange;
+	padding: 10px;
+}
+</style>
